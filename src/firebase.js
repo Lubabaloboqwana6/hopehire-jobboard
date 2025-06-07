4202fda25fadfee83c81e48e0d8c9985a7444d59
Lubabaloboqwana6 <lubabaloboqwana6@gmail.com>
2025-06-07 09:21:10 +0200

    added .env file

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDPLmrk7AbeZdAQHmGkZJfpC4Fp0ltyKE",
-  authDomain: "career-path-builder-cf8df.firebaseapp.com",
-  projectId: "career-path-builder-cf8df",
-  storageBucket: "career-path-builder-cf8df.firebasestorage.app",
-  messagingSenderId: "853534340072",
-  appId: "1:853534340072:web:b2f548a8f067bd609fea5d",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
